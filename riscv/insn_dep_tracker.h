54ff26fec71ebb73c5f2d3150be79eae1a687231
Jiayang Li <admin@0x10c.pw>
2021-02-04 23:45:22 -0500

    add a instruction poisoning tracker implementation
    Signed-off-by: Jiayang Li <admin@0x10c.pw>

--- riscv/insn_dep_tracker.h
+++ riscv/insn_dep_tracker.h
@@ -10,11 +10,12 @@
 #include <cassert>
 #include "debug_tracer.h"
 
+typedef uint64_t data_addr_t;
+typedef uint64_t inst_addr_t;
+typedef size_t access_size_t;
+
 class mem_poisoning_tracker {
 public:
-  typedef uint64_t data_addr_t;
-  typedef uint64_t inst_addr_t;
-  typedef uint8_t access_size_t;
   struct node_stat_t {
     std::set<inst_addr_t> producer_pc_set;
     size_t ref_cnt;
@@ -90,7 +91,6 @@ public:
 };
 
 class reg_poisoning_tracker {
-  typedef uint64_t inst_addr_t;
   typedef std::set<inst_addr_t> producer_pc_set_t;
 private:
   std::vector<producer_pc_set_t> m_reg_producer_set;
@@ -117,20 +117,104 @@ public:
   bool is_poisoned(size_t reg_no) {
     return !m_reg_producer_set[reg_no].empty();
   }
-
 };
 
-class insn_dep_tracker {
+
+class insn_poisoning_tracker {
+private:
+  enum tracking_mode_t {
+    POISONING,
+    PROPAGATE,
+    STOP
+  } m_tracking_mode;
+
+  static const int N_INT_REG = 32;
+  static const int N_FP_REG = 32;
+
+  mem_poisoning_tracker m_mem_tracker;
+  // The first N_INT_REG slot is for integer register
+  // Then the remaining N_FP_REG slot is for FP register
+  reg_poisoning_tracker m_regs_tracker;
+
+
 public:
-  void enter_poisoning_mode();
+  insn_poisoning_tracker() : m_mem_tracker(), m_regs_tracker(N_INT_REG + N_FP_REG) {
+    m_tracking_mode = STOP;
+  }
 
-  void enter_propagate_mode();
+  void update(const insn_record_t &insn) {
+    switch (m_tracking_mode) {
+      case POISONING: {
+        auto insn_producer = get_insn_src_producer_set(insn);
+        insn_producer.insert(insn.pc);
+        set_insn_dst_producer_set(insn, insn_producer);
+        break;
+      }
 
-  void import_poisoning_state();
+      case PROPAGATE: {
+        if (get_insn_src_poisoned(insn)) {
+          auto insn_producer = get_insn_src_producer_set(insn);
+          insn_producer.insert(insn.pc);
+          set_insn_dst_producer_set(insn, insn_producer);
+        }
+        break;
+      }
 
-  void export_poisoning_state();
+      case STOP:
+      default:
+        break;
+    }
+  };
 
-  void update(const insn_record_t &insn);
+  void enter_poisoning_mode() { m_tracking_mode = POISONING; };
+
+  void enter_propagate_mode() { m_tracking_mode = PROPAGATE; };
+
+  void stop_tracking() { m_tracking_mode = STOP; };
+
+  bool get_insn_src_poisoned(const insn_record_t &insn) {
+    for (const auto &rs_rec : insn.rs_rec) {
+      if (rs_rec.valid) {
+        size_t reg_offset = rs_rec.fpr ? N_INT_REG : 0;
+        if (m_regs_tracker.is_poisoned(reg_offset + rs_rec.n))
+          return true;
+      }
+    }
+    if (insn.mem_rec.valid && !insn.mem_rec.write) {
+      if (m_mem_tracker.is_poisoned(insn.mem_rec.vaddr, insn.mem_rec.op_size))
+        return true;
+    }
+    return false;
+  }
+
+  std::set<inst_addr_t> get_insn_src_producer_set(const insn_record_t &insn) {
+    std::set<inst_addr_t> producer_set;
+    for (const auto &rs_rec : insn.rs_rec) {
+      if (rs_rec.valid) {
+        size_t reg_offset = rs_rec.fpr ? N_INT_REG : 0;
+        auto rsrc_p_set = m_regs_tracker.query_producer(reg_offset + rs_rec.n);
+        if (!rsrc_p_set.empty()) producer_set.insert(rsrc_p_set.begin(), rsrc_p_set.end());
+      }
+    }
+    if (insn.mem_rec.valid && !insn.mem_rec.write) {
+      auto msrc_p_set = m_mem_tracker.query_producer(insn.mem_rec.vaddr, insn.mem_rec.op_size);
+      if (!msrc_p_set.empty()) producer_set.insert(msrc_p_set.begin(), msrc_p_set.end());
+    }
+
+    return producer_set;
+  }
+
+  void set_insn_dst_producer_set(const insn_record_t &insn, const std::set<inst_addr_t> &insn_src_producer_set) {
+    for (const auto &rd_rec : insn.rd_rec) {
+      if (rd_rec.valid) {
+        size_t reg_offset = rd_rec.fpr ? N_INT_REG : 0;
+        m_regs_tracker.poisoning(rd_rec.n, insn_src_producer_set);
+      }
+    }
+    if (insn.mem_rec.valid && insn.mem_rec.write) {
+      m_mem_tracker.poisoning(insn.mem_rec.vaddr, insn.mem_rec.op_size, insn_src_producer_set);
+    }
+  }
 };
 
 
