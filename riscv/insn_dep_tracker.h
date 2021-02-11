566a0e71cc57792fb576940788585294a95ab564
Jiayang Li <admin@0x10c.pw>
2021-02-11 17:11:29 -0500

    added a CFS impl introduced in the TCI paper
    Signed-off-by: Jiayang Li <admin@0x10c.pw>

--- riscv/insn_dep_tracker.h
+++ riscv/insn_dep_tracker.h
@@ -9,6 +9,9 @@
 #include <list>
 #include <cassert>
 #include "debug_tracer.h"
+#include "reconv_predictor.h"
+
+#define CFS_ENABLE_INVALID_RECONV_MERGE
 
 typedef uint64_t data_addr_t;
 typedef uint64_t insn_addr_t;
@@ -235,5 +238,169 @@ public:
   }
 };
 
+class control_flow_stack {
+public:
+  struct cfs_frame {
+    ssize_t call_depth;
+    insn_addr_t reconv_pc;
+    size_t n_cd_retired;
+    size_t branch_stack_top; // inclusive
+    size_t branch_stack_bottom; // inclusive
+  };
+private:
+  static const size_t DEFAULT_STACK_SIZE = 64;
+  static const size_t CFS_CD_REGION_MAX = 128;
+  ssize_t m_curr_call_depth;
+  std::vector<insn_addr_t> m_active_branch_stack;
+  std::vector<cfs_frame> m_cfs;
+
+  control_flow_stack() {
+    m_curr_call_depth = 0;
+    m_cfs.resize(DEFAULT_STACK_SIZE);
+    m_active_branch_stack.resize(DEFAULT_STACK_SIZE);
+  };
+
+  void reset() {
+    m_curr_call_depth = 0;
+    m_cfs.clear();
+    m_active_branch_stack.clear();
+  }
+
+  void increase_call_depth() {
+    ++m_curr_call_depth;
+  };
+
+  void decrease_call_depth() {
+    --m_curr_call_depth;
+  };
+
+  // push a branch and its reconv point into cfs
+  void push(insn_addr_t br_pc, insn_addr_t reconv_pc) {
+    if (m_cfs.empty()) {
+      assert(m_active_branch_stack.empty());
+      m_active_branch_stack.push_back(br_pc);
+      cfs_frame new_cfs_frame = {
+        .call_depth = m_curr_call_depth,
+        .reconv_pc = reconv_pc,
+        .n_cd_retired = 0,
+        .branch_stack_top = 0,
+        .branch_stack_bottom = 0,
+      };
+      m_cfs.push_back(new_cfs_frame);
+    } else {
+      auto &cfs_tos = m_cfs.back();
+
+      if (m_curr_call_depth == cfs_tos.call_depth && reconv_pc == cfs_tos.reconv_pc) {
+        /* in the same call depth, if reconv PC is the same, implicitly merge it into the current cfs top frame */
+        m_active_branch_stack.push_back(br_pc);
+        ++cfs_tos.branch_stack_top;
+      } else {
+        /* else, create a new CFS frame
+         * here we don't need to treat two special reconv PC differently, RECONV_POINT_INVALID and RECONV_POINT_RETURN,
+         * as those two PC cannot be met in the normal execution
+         */
+        auto branch_stack_idx = m_active_branch_stack.size();
+        m_active_branch_stack.push_back(br_pc);
+        cfs_frame new_cfs_frame = {
+          .call_depth = m_curr_call_depth,
+          .reconv_pc = reconv_pc,
+          .n_cd_retired = 0,
+          .branch_stack_top = branch_stack_idx,
+          .branch_stack_bottom = branch_stack_idx,
+        };
+        m_cfs.push_back(new_cfs_frame);
+      }
+    }
+  }
+
+  // pop the top frame from CFS
+  void pop() {
+    if (m_cfs.empty())
+      return;
+    auto &cfs_tos = m_cfs.back();
+    // pop record from the active branch stack
+    auto branch_to_pop = cfs_tos.branch_stack_top - cfs_tos.branch_stack_bottom + 1;
+    for (auto i = branch_to_pop; i >= 0; --i) {
+      m_active_branch_stack.pop_back();
+    }
+    m_cfs.pop_back();
+  }
+
+  bool check_reconv(uint64_t curr_pc) const {
+    if (m_cfs.empty())
+      return false;
+    const auto &cfs_tos = m_cfs.back();
+    return (m_curr_call_depth == cfs_tos.call_depth && curr_pc == cfs_tos.reconv_pc);
+  }
+
+  bool good() const {
+    if (m_cfs.empty()) {
+      assert(m_active_branch_stack.empty());
+      return true;
+    }
+
+    const auto &cfs_tos = m_cfs.back();
+    if (cfs_tos.n_cd_retired > CFS_CD_REGION_MAX) {
+      // the reconv pc is not seen before reaching the max Cd region threshold
+      return false;
+    }
+
+    if (m_curr_call_depth < cfs_tos.call_depth) {
+      // the reconv pc is not seen before function return
+      return false;
+    }
+
+    return true;
+  }
+
+  const std::vector<insn_addr_t> &get_active_branches() const { return m_active_branch_stack; };
+
+  void on_insn_retire(insn_addr_t insn_pc) {
+    if (m_cfs.empty())
+      return;
+    auto &cfs_tos = m_cfs.back();
+    ++cfs_tos.n_cd_retired;
+    if (check_reconv(insn_pc)) {
+      pop();
+    }
+  };
+
+  void on_branch(insn_addr_t br_pc, insn_addr_t reconv_pc) {
+    on_insn_retire(br_pc);
+
+#ifdef CFS_ENABLE_INVALID_RECONV_MERGE
+    if (m_cfs.empty()) {
+      push(br_pc, reconv_pc);
+    } else {
+      auto cfs_top = m_cfs.back();
+      if (m_curr_call_depth == cfs_top.call_depth && cfs_top.reconv_pc == RECONV_POINT_INVALID) {
+        push(br_pc, cfs_top.reconv_pc);
+      } else {
+        push(br_pc, reconv_pc);
+      }
+    }
+#else
+    push(br_pc, reconv_pc);
+#endif
+
+  };
+
+  void on_fcall(uint64_t fcall_pc) {
+    on_insn_retire(fcall_pc);
+    increase_call_depth();
+  }
+
+  void on_fret(uint64_t fret_pc) {
+    on_insn_retire(fret_pc);
+    if (check_reconv(RECONV_POINT_RETURN)) {
+      pop();
+    }
+    decrease_call_depth();
+  }
+
+  void on_other(uint64_t insn_pc) {
+    on_insn_retire(insn_pc);
+  }
+};
 
 #endif //RISCV_ISA_SIM_INSN_DEP_TRACKER_H
